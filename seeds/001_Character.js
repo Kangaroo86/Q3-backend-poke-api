f5ecfc40683901cc6f7355505bc2de83718e39c1
Cang Le <cang.b.le@gmail.com>
2017-10-27 14:24:10 -0700

    pokebe

--- seeds/001_Character.js
+++ seeds/001_Character.js
@@ -1,78 +1,85 @@
 exports.seed = function(knex, Promise) {
-  return knex('Character').del().then(() => {
-    return knex('Character').insert([
-      {
-        id: 1,
-        name: 'Bulbasaur'
-      },
-      {
-        id: 4,
-        name: 'Charmander'
-      },
-      {
-        id: 7,
-        name: 'Squirtle'
-      },
-      {
-        id: 152,
-        name: 'Chikorita'
-      },
-      {
-        id: 155,
-        name: 'Cyndaquil'
-      },
-      {
-        id: 158,
-        name: 'Totodile'
-      },
-      {
-        id: 252,
-        name: 'Treecko'
-      },
-      {
-        id: 255,
-        name: 'Torchic'
-      },
-      {
-        id: 258,
-        name: 'Mudkip'
-      },
-      {
-        id: 150,
-        name: 'Mewtwo'
-      },
-      {
-        id: 151,
-        name: 'Mew'
-      },
-      {
-        id: 25,
-        name: 'Pikachu'
-      },
-      {
-        id: 144,
-        name: 'Articuno'
-      },
-      {
-        id: 145,
-        name: 'Zapdo'
-      },
-      {
-        id: 146,
-        name: 'Moltres'
-      },
-      {
-        id: 243,
-        name: 'Raikou'
-      },
-      {
-        id: 244,
-        name: 'Entei'
-      },
-      {
-        id: 245,
-        name: 'Suicune'
-      }
-    ]);
-  });
+  return knex('Character')
+    .del()
+    .then(() => {
+      return knex('Character').insert([
+        {
+          name: 'Bulbasaur',
+          pokemonId: 1
+        },
+        {
+          name: 'Charmander',
+          pokemonId: 4
+        },
+        {
+          name: 'Squirtle',
+          pokemonId: 7
+        },
+        {
+          name: 'Ditto',
+          pokemonId: 132
+        },
+        {
+          name: 'Golem',
+          pokemonId: 76
+        },
+        {
+          name: 'Nidoqueen',
+          pokemonId: 31
+        },
+        {
+          name: 'Snorlax',
+          pokemonId: 143
+        },
+        {
+          name: 'Groudon',
+          pokemonId: 383
+        },
+        {
+          name: 'Xerneas',
+          pokemonId: 716
+        },
+        {
+          name: 'Mewtwo',
+          pokemonId: 150
+        },
+        {
+          name: 'Mew',
+          pokemonId: 151
+        },
+        {
+          name: 'Pikachu',
+          pokemonId: 25
+        },
+        {
+          name: 'Articuno',
+          pokemonId: 144
+        },
+        {
+          name: 'Zapdo',
+          pokemonId: 145
+        },
+        {
+          name: 'Moltres',
+          pokemonId: 146
+        },
+        {
+          name: 'Raikou',
+          pokemonId: 243
+        },
+        {
+          name: 'Jigglypuff',
+          pokemonId: 39
+        },
+        {
+          name: 'Gyarados',
+          pokemonId: 130
+        }
+      ]);
+    })
+    .then(() =>
+      knex.raw(
+        `SELECT setval('"Character_id_seq"', (SELECT MAX("id") FROM "Character"))`
+      )
+    );
 };
