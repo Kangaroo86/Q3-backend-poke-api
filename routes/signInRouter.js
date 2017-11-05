11b2b5f6a9b7f75736fff927fd4458a77072fa13
Cang Le <cang.b.le@gmail.com>
2017-11-05 14:34:09 -0800

    backend

--- routes/signInRouter.js
+++ routes/signInRouter.js
@@ -2,55 +2,65 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 const bcrypt = require('bcrypt');
-const { promisify } = require('util');
 const env = require('./../env');
-const { JWT_KEY } = require('../env');
 var jwt = require('jsonwebtoken');
-const signJWT = promisify(jwt.sign);
+const { JWT_KEY } = require('../env');
+//const { promisify } = require('util');
+//const signJWT = promisify(jwt.sign);
+
+router.get('/token', (request, response, next) => {
+  if (request.cookie.token) {
+    jwt.verify(request.cookie.token, env.JWT_KEY, (err, response) => {
+      if (response) response.status(200).json(true);
+      else {
+        response.status(200).json(false);
+      }
+    });
+  } else {
+    response.status(200).json(false);
+  }
+});
 
 router.post('/token', (request, response, next) => {
-  if (!request.body.email) {
+  if (!request.body.name) {
     response.set('Content-Type', 'text/plain');
-    response.status(400).send('Email must not be blank');
+    response.status(400).send('Name must not be blank');
     return;
   }
-
   if (!request.body.password) {
     response.set('Content-Type', 'text/plain');
     response.status(400).send('Password must not be blank');
     return;
   }
-  bcrypt
-    .hash(request.body.password, 12)
-    .then(hashedPassword => {
-      //console.log(hashedPassword);
-      return knex('User').where('email', request.body.email).then(record => {
-        return bcrypt
-          .compare(request.body.password, record[0].hashedPassword)
-          .then(response => {
-            //console.log(request.body.password, record[0].hashedPassword, response);
-            if (!response) {
-              response.set('Content-Type', 'text/plain');
 
-              response.status(400).send('Bad email or password');
-              return;
-            } else {
-              let token = jwt.sign(
-                {
-                  id: record[0].id,
-                  name: record[0].name,
-                  email: record[0].email
-                },
-                env.JWT_KEY
-              );
-              response.cookie('token', token, { httpOnly: true }).json({
+  return knex('User')
+    .where('name', request.body.name)
+    .then(record => {
+      return bcrypt
+        .compare(request.body.password, record[0].hashedPassword)
+        .then(response => {
+          if (!response) {
+            response.set('Content-Type', 'text/plain');
+
+            response.status(400).send('Bad email or password');
+            return;
+          } else {
+            let token = jwt.sign(
+              {
                 id: record[0].id,
                 name: record[0].name,
                 email: record[0].email
-              });
-            }
-          });
-      });
+              },
+              env.JWT_KEY
+            );
+            response.cookie('token', token, { httpOnly: true }).json({
+              id: record[0].id,
+              name: record[0].name,
+              email: record[0].email,
+              token: token
+            });
+          }
+        });
     })
     .catch(() => {
       response.set('Content-Type', 'text/plain');
@@ -58,11 +68,58 @@ router.post('/token', (request, response, next) => {
     });
 });
 
+router.delete('/token', (request, response) => {
+  response.clearCookie('token').json(true);
+});
+
 module.exports = router;
 
+// router.post('/token', (request, response, next) => {
+//   if (!request.body.password) {
+//     response.set('Content-Type', 'text/plain');
+//     response.status(400).send('Password must not be blank');
+//     return;
+//   }
+//   bcrypt
+//     .hash(request.body.password, 12)
+//     .then(hashedPassword => {
+//       console.log('does hasedpassword-------', hashedPassword);
+//       return knex('User').where('email', request.body.email).then(record => {
+//         return bcrypt
+//           .compare(request.body.password, record[0].hashedPassword)
+//           .then(response => {
+//             //console.log(request.body.password, record[0].hashedPassword, response);
+//             if (!response) {
+//               response.set('Content-Type', 'text/plain');
 //
+//               response.status(400).send('Bad email or password');
+//               return;
+//             } else {
+//               let token = jwt.sign(
+//                 {
+//                   id: record[0].id,
+//                   name: record[0].name,
+//                   email: record[0].email
+//                 },
+//                 env.JWT_KEY
+//               );
+//               response.cookie('token', token, { httpOnly: true }).json({
+//                 id: record[0].id,
+//                 name: record[0].name,
+//                 email: record[0].email
+//               });
+//             }
+//           });
+//       });
+//     })
+//     .catch(() => {
+//       response.set('Content-Type', 'text/plain');
+//       response.status(400).send('Bad email or password');
+//     });
+// });
 //
-//
+// module.exports = router;
+
 // router.post('/token', (request, response, next) => {
 //   const scope = {};
 //   //const email = request.body.email;
