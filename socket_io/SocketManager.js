d1dc75259d7d0275152576add4e42919ea0c115a
Cang Le <cang.b.le@gmail.com>
2018-03-19 01:12:34 -0700

    refactored

--- socket_io/SocketManager.js
+++ socket_io/SocketManager.js
@@ -8,6 +8,7 @@ module.exports = io => {
       if (battleId) {
         socket.join(battleId);
         updateMessage(battleId);
+        updateBattleState(battleId);
       }
     });
 
@@ -46,45 +47,27 @@ module.exports = io => {
         });
     }
 
+    function updateBattleState(battleId) {
+      knex('Battle').where('id', battleId).select('*').then(obj => {
+        console.log('what is updated state--------------', obj[0].state);
+        io.in(battleId).emit('UPDATED_BATTLE_STATE', obj[0].state);
+      });
+    }
+
     /*SET_BATTLE_STATE*/
     socket.on('STATE_UPDATED', stateObj => {
       setBattleState(stateObj);
     });
 
-    //TODO not compatible with socket io yet
     function setBattleState(stateObj) {
-      console.log('setBattleState--------------', stateObj);
       knex('Battle')
         .where('id', stateObj.battle_id)
         .update({ state: stateObj }, '*')
         .then(obj => {
-          console.log('obj---------', obj);
           io
             .in(obj[0].state.battle_id)
             .emit('UPDATED_BATTLE_STATE', obj[0].state);
         });
     }
-
-    //***VERIFY USERNAME***//
-    // socket.on(VERIFY_USER, (name, userId, battleId) => {
-    //   //goes on the HOME Page
-    //   let user = {};
-    //   if (userId === 'null') {
-    //     user = createUser({ name: name });
-    //     socket.emit(USER_CREATED, user);
-    //     connectedUsers = addUser(connectedUsers, user);
-    //   } else {
-    //     user.id = userId;
-    //     user.name = name;
-    //   }
-    //   socket.user = user;
-    //   //socket.join(socket.room);
-    //
-    //   if (battleId !== 'null') {
-    //     getMessage(battleId);
-    //   }
-    //
-    //   io.emit(USER_CONNECTED, connectedUsers);
-    // });
   });
 };
